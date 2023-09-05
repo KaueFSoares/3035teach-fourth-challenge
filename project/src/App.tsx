3cc3947bfdae1616bd699025c2ec97d65b6cf0e3
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-05 20:12:06 -0300

    start: creates and clean up the project and install dependencies
    dependencies:
    tailwindcss
    react-router-dom
    
    config:
    sets up color palette on tailwind
    sets up eslint rules
    sets up the default font as Montserrat
    sets up the favicon and app title

---
+++ project/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <h1 className="text-orange font-extrabold text-4xl">teste2</h1>
+    </>
+  )
+}
+
+export default App
---
+++ project/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "dark-blue": "#05478A",
+        "light-blue": "#0070E0",
+        "orange": "#FC8621",
+        "gray": "#BDBDBD",
+        "dark-green": "#00420C",
+      },
+      screens: {
+        xsm: "380px",
+      },
+    },
+  },
+  plugins: [],
+}

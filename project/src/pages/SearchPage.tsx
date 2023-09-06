245ef27f4ea277f76988c236155dfd069714d93c
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-06 08:01:58 -0300

    feat: adds the message component to the search page

--- project/src/pages/SearchPage.tsx
+++ project/src/pages/SearchPage.tsx
@@ -1,21 +1,65 @@
 import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
 import { search } from "../services/search.service"
 import Loading from "../components/Loading"
+import Message from "../components/Message"
 
 const SearchPage = () => {
+  const navigate = useNavigate()
+
   const [ username, setUsername ] = useState<string>("")
 
   const [ loading, setLoading ] = useState<boolean>(false)
+
+  const [ message, setMessage ] = useState<string>("")
+
+  const [ showMessage, setShowMessage ] = useState<boolean>(false)
 
   const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
     setLoading(true)
 
+    if (!validateUsername(username)) {
+      setMessage("Please, enter a valid username.")
+      setShowMessage(true)
+      setLoading(false)
+
+      return
+    }
+
     search(username)
-      .then(() => {
+      .then((res) => {
         setLoading(false)
+
+        if (res === "ok") {
+          navigate(`/profile/${username}`)
+          
+          return
+        }
+
+        if (res === "notfound") {
+          setMessage("We couldn't find this user.")
+          setShowMessage(true)
+
+          return 
+        }
+
+        setMessage("Something went wrong. Please try again.")
+        setShowMessage(true)
       })
+  }
+
+  const handleOnCloseMessage = () => {
+    setShowMessage(false)
+  }
+
+  const validateUsername = (username: string) => {
+    if (username.length === 0) {
+      return false
+    }
+
+    return true
   }
 
   return (
@@ -35,6 +79,7 @@ const SearchPage = () => {
               className="flex flex-col w-3/4 sm:w-1/2 md:w-1/3 lg:w-3/5"
               onSubmit={handleOnSubmit}
             >
+              {showMessage && <Message text={message} handleOnClose={handleOnCloseMessage}/>}
               <div className="flex flex-col w-full">
                 <label htmlFor="user-input" className="text-xl mb-2 font-semibold lg:text-lg">User</label>
                 <input 
@@ -43,13 +88,13 @@ const SearchPage = () => {
                   type="text" 
                   id="user-input"
                   placeholder="Enter your Github username"
-                  className="w-full border-2 border-gray rounded-md p-2 mb-4 text-md outline-none focus:border-dark-blue focus:bg-dark-blue/[.03] lg:text-sm lg:mb-8"
+                  className="w-full border-2 border-gray rounded-md p-2 mb-4 text-md outline-none focus:border-dark-blue focus:bg-dark-blue/[.1] lg:text-sm lg:mb-8"
                 />
               </div>
 
               <button 
                 type="submit"
-                className="bg-dark-blue text-white font-bold text-2xl rounded-md p-2 lg:text-lg"
+                className="bg-dark-blue text-white font-bold text-2xl rounded-md p-2 lg:text-lg hover:bg-dark-blue/[.9] transition-all"
               >
             Search
               </button>

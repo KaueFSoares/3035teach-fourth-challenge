d7566ceb43daf15a4df37c7504a8b5435619c7fa
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-05 22:43:50 -0300

    feat: sets the loading logic on the search page

--- project/src/pages/SearchPage.tsx
+++ project/src/pages/SearchPage.tsx
@@ -1,6 +1,23 @@
-
+import React, { useState } from "react"
+import { search } from "../services/search.service"
+import Loading from "../components/Loading"
 
 const SearchPage = () => {
+  const [ username, setUsername ] = useState<string>("")
+
+  const [ loading, setLoading ] = useState<boolean>(false)
+
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    setLoading(true)
+
+    search(username)
+      .then(() => {
+        setLoading(false)
+      })
+  }
+
   return (
     <main className="w-full h-screen flex flex-col lg:flex-row">
       <div className="bg-dark-blue w-full h-2/5 flex items-center justify-center lg:h-full lg:w-3/5">
@@ -8,25 +25,36 @@ const SearchPage = () => {
       </div>
 
       <div className="w-full h-1/2 flex flex-col items-center justify-center lg:h-full lg:w-2/5">
-        <h1 className="font-extrabold text-3xl mb-8 lg:text-3xl">Welcome!</h1>
-        <form action="" className="flex flex-col w-3/4 sm:w-1/2 md:w-1/3 lg:w-3/5">
-          <div className="flex flex-col w-full">
-            <label htmlFor="user-input" className="text-xl mb-2 font-semibold lg:text-lg">User</label>
-            <input 
-              type="text" 
-              id="user-input"
-              placeholder="Enter your Github username"
-              className="w-full border-2 border-gray rounded-md p-2 mb-4 text-md outline-none focus:border-dark-blue focus:bg-dark-blue/[.03] lg:text-sm lg:mb-8"
-            />
-          </div>
+        {loading ? (
+          <Loading />
+        ) : (
+          <>
+            <h1 className="font-extrabold text-3xl mb-8 lg:text-3xl">Welcome!</h1>
+            <form 
+              action="" 
+              className="flex flex-col w-3/4 sm:w-1/2 md:w-1/3 lg:w-3/5"
+              onSubmit={handleOnSubmit}
+            >
+              <div className="flex flex-col w-full">
+                <label htmlFor="user-input" className="text-xl mb-2 font-semibold lg:text-lg">User</label>
+                <input 
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  type="text" 
+                  id="user-input"
+                  placeholder="Enter your Github username"
+                  className="w-full border-2 border-gray rounded-md p-2 mb-4 text-md outline-none focus:border-dark-blue focus:bg-dark-blue/[.03] lg:text-sm lg:mb-8"
+                />
+              </div>
 
-          <button 
-            type="submit"
-            className="bg-dark-blue text-white font-bold text-2xl rounded-md p-2 lg:text-lg"
-          >
+              <button 
+                type="submit"
+                className="bg-dark-blue text-white font-bold text-2xl rounded-md p-2 lg:text-lg"
+              >
             Search
-          </button>
-        </form>
+              </button>
+            </form></>
+        )}
       </div>
     </main>
   )

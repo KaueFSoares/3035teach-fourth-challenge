a9b78d286099222299a407fbd9054f460a98bfe1
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-06 09:39:12 -0300

    feat: implements the loading and not found logic on profile page

--- project/src/pages/ProfilePage.tsx
+++ project/src/pages/ProfilePage.tsx
@@ -1,10 +1,38 @@
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import { ProfileData, RepoData, getData } from "../services/profile.service"
+import Loading from "../components/Loading"
 
 const ProfilePage = () => {
-  const { userName } = useParams<{userName: string}>()
+  const navigate = useNavigate()
   
+  const { userName } = useParams<{userName: string}>()
+
+  const [ profile, setProfile ] = useState<ProfileData>()
+  const [ repos, setRepos ] = useState<RepoData[]>()
+
+
+  useEffect(() => {
+    if (userName) {
+      getData(userName).then((data) => {
+        if (data[0] === "notfound" || data[0] === "error") {
+          navigate("/notfound")
+        }
+
+        setProfile(data[1])
+        setRepos(data[2])
+      })
+    }
+  }, [ userName ])
+
   return (
-    <div>This the {userName} profile page!</div>
+    <main className="w-full h-screen">
+      {(profile && repos) ? (
+        <p>carregou</p>
+      ) : (
+        <Loading />
+      )}
+    </main>
   )
 }
 

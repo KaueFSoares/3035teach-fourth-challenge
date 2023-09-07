37dd2b6ca5eed07fd6d645b1c7dad3f6fc948620
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-06 22:25:48 -0300

    style: changes the header padding on mobile

--- project/src/components/Header.tsx
+++ project/src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <header className="h-[9%] py-5 px-10 bg-white shadow-md w-full">
+    <header className="h-[9%] py-5 px-5 lg:px-10 bg-white shadow-md w-full">
       <Link to="/" className="h-full">
         <img src="/bg-white-logo.png" alt="W Tech Logo" className="h-full" />
       </Link>

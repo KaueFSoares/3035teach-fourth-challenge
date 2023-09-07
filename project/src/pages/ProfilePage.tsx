fec7999b461f39b9a255791657cb4dfd44ec0c24
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-06 22:33:44 -0300

    refactor: sets a default text for when there is no name or bio

--- project/src/pages/ProfilePage.tsx
+++ project/src/pages/ProfilePage.tsx
@@ -27,12 +27,12 @@ const ProfilePage = () => {
   }, [ navigate, userName ])
 
   return (
-    <main className="w-full min-h-screen bg-gray/[.5] flex flex-col items-center justify-start ">
+    <main className="w-full min-h-screen h-screen bg-gray/[.5] flex flex-col items-center justify-start ">
       {(profile && repos) ? (
         <>
           <Header />
-          <div className="w-[95%] h-[100%] bg-white box-border shadow-sm rounded-xl mt-4 md:mt-6">
-            <section className="p-5 lg:p-8 mb">
+          <div className="w-[95%] h-[100%] bg-white box-border shadow-sm rounded-xl my-4 md:my-6">
+            <section className="p-5 lg:p-8 mb-8">
               <h2 className="font-extrabold text-xl mb-4 md:text-2xl md:mb-8 xl:text-3xl">Profile info</h2>
 
               <div className="grid grid-cols-3 gap-4 sm:grid-cols-3 lg:gap-y-0 xl:grid-cols-4 xl:gap-x-8
@@ -47,12 +47,12 @@ const ProfilePage = () => {
                 <div className="col-span-2 sm:col-span-2 sm:row-span-1 xl:col-span-3
                                 flex flex-col justify-center">
                   <p className="font-extralight text-sm mb-1">Name</p>
-                  <p className="font-semibold mb-2 text-base">{profile.name}</p>
+                  <p className="font-semibold mb-2 text-base">{profile.name ? profile.name : "No name"}</p>
                 </div>
 
                 <div className="col-span-3 sm:col-span-2 sm:row-span-1 xl:col-span-3">
                   <p className="font-extralight text-sm mb-1">Bio</p>
-                  <p className="text-sm mb-1 text-justify pr-2">{profile.bio}</p>
+                  <p className="text-sm mb-1 text-justify pr-2">{profile.bio ? profile.bio : "No bio"}</p>
                 </div>
               </div>
 

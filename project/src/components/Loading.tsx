a09bd31d222c0bec21178e7c36c36f5002da3550
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-09-07 22:54:44 -0300

    feat: adds the work "loading" to the loading component

--- project/src/components/Loading.tsx
+++ project/src/components/Loading.tsx
@@ -1,12 +1,13 @@
 const Loading = () => {
   return (
-    <div className="w-full h-full flex items-center justify-center">
+    <div className="w-full h-full flex items-center justify-center flex-col">
       <div className="relative w-1/6 lg:w-[6%]">
         <img src="/out-circle.png" alt="" className="w-full"/>
         <div className="w-2/3 h-2/3 m-auto animate-spin-slow absolute inset-0">
           <img src="/in-circle.png" alt="" className="w-full h-full rounded-full" />
         </div>
       </div>
+      <h2 className="mt-4 font-bold text-light-blue text-xl lg:text-2xl">Loading</h2>
     </div>
   )
 }
